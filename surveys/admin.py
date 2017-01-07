c9132e8fe6fc5d11189aac7ae99785573cb18c29
Gameli Ladzekpo <gameli.ladzekpo@gmail.com>
2017-01-07 13:19:32 +0000

    setup admin

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from nested_inline.admin import NestedStackedInline, NestedModelAdmin
+
+from .models import Survey, Question, Answer, Choice, Response
+
+
+class ChoiceInline(NestedStackedInline):
+    model = Choice
+    extra = 1
+
+class QuestionInline(NestedStackedInline):
+    model = Question
+    extra = 0
+
+    inlines = [ChoiceInline]
+    search_fields = ['question']
+
+@admin.register(Survey)
+class SurveyAdmin(NestedModelAdmin):
+    inlines = [QuestionInline]
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 1
+
+@admin.register(Response)
+class ResponseAdmin(admin.ModelAdmin):
+    model = Response
+
+    inlines = [AnswerInline]
+
--- surveys/models.py
+++ surveys/models.py
@@ -1,10 +1,8 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
 
 class Survey(models.Model):
+
     name = models.CharField(max_length=150)
     description = models.TextField(null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
@@ -15,41 +13,38 @@ class Survey(models.Model):
 
 
 class Question(models.Model):
-    question_text = models.TextField()
+
+    text = models.TextField()
     required = models.BooleanField(default=True)
-    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, related_name="questions")
 
     TEXT = 'TEXT'
-    RADIO = 'RADIO'
+    SELECT_ONE = 'SELECT ONE'
     SELECT_MULTIPLE = 'SELECT MULTIPLE'
     INTEGER = 'INTEGER'
     QUESTION_TYPE_CHOICES = (
         (TEXT, 'Text'),
-        (RADIO, 'Radio'),
+        (SELECT_ONE, 'Select one'),
         (SELECT_MULTIPLE, 'Select multiple'),
         (INTEGER, 'Integer'),
     )
-    question_type = models.CharField(
+    type = models.CharField(
         max_length=50,
         choices=QUESTION_TYPE_CHOICES,
         default=TEXT,
     )
 
-    # To do:
-    # Figure out if this is best approach or if we need to create a separate model instance for this
-    choices = models.TextField(blank=True, null=True, help_text='Enter comma separated values')
-
     def __str__(self):
-        return self.question_text
+        return self.text
 
-# To do:
-# Have this model inline with question so user can enter the choices as they fill the questions
-# Have a way to let the user know that this is not necessary if the question type selected is Text
+
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+
+    text = models.CharField(max_length=200, blank=True, null=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="choices")
+
     def __str__(self):
-        return self.choice_text
+        return self.text
 
 
 class Response(models.Model):
@@ -57,20 +52,20 @@ class Response(models.Model):
     This provides a way to get a collection of questions and answers
     from a particular user for a particular survey.
     """
-    response_id = models.CharField(max_length=150)
-    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    responder_id = models.CharField(max_length=150)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, related_name="responses")
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.response_id
+        return self.responder_id
 
 
 class Answer(models.Model):
-    answer_body = models.TextField(blank=True, null=True)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    response = models.ForeignKey(Response, on_delete=models.CASCADE)
 
+    body = models.TextField(blank=True, null=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="answers")
+    response = models.ForeignKey(Response, on_delete=models.CASCADE, null=True,  related_name="answers")
 
     def __str__(self):
-        return self.answer_body
+        return self.body
