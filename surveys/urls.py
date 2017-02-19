eec7b2ee66bd693610d459d8febe922b66984a16
Gameli Ladzekpo <gameli.ladzekpo@gmail.com>
2017-02-19 23:43:02 +0000

    update static with email

--- surveys/urls.py
+++ surveys/urls.py
@@ -8,9 +8,13 @@ urlpatterns = [
     url(r'^$',  views.home, name='home'),
     url(r'^about/$',  views.about, name='about'),
     url(r'^blog/$',  views.blog, name='blog'),
-    url(r'^sign-up/$',  views.get_signup, name='signup'),
     url(r'^privacy-policy/$',  views.privacypolicy, name='privacypolicy'),
     url(r'^pricing/$',  views.pricing, name='pricing'),
+    url(r'^survey-form/$',  views.pricing, name='survey-form'),
+
+    url(r'^sign-up/$',  views.success, name='signup'),
+    url(r'^success/$',  views.success, name='success'),
+    url(r'^thanks/$',  views.thanks),
     # url(r'^contact/$',  views.contact, name='contact'),
     # ex: /survey/5/
     url(r'^survey/(?P<survey_id>[0-9]+)/$', views.survey, name='survey'),
