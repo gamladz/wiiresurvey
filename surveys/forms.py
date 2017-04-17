a1fa22134dbf3dd6dff67beccc7974f57115b77a
Gameli Ladzekpo <gameli.ladzekpo@gmail.com>
2017-04-17 11:56:17 +0100

    update landing age how it works

--- surveys/forms.py
+++ surveys/forms.py
@@ -1,5 +1,14 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from surveys.models import Survey, Question, Choice, Response, Answer
+
+class SignUpForm(UserCreationForm):
+    birth_date = forms.DateField(help_text='Required. Format: YYYY-MM-DD')
+
+    class Meta:
+        model = User
+        fields = ('username', 'birth_date', 'password1', 'password2', )
 
 
 class ContactForm(forms.Form):
