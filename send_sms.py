3a90641a203a196f4afd5eaf1e04b9c8500eac18
Gameli Ladzekpo <gameli.ladzekpo@gmail.com>
2017-06-01 14:48:20 +0100

    update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -12,5 +12,5 @@ client = Client(account_sid, auth_token)
 client.messages.create(
     to=dotenv.get('MY_PHONE_NUMBER'),
     from_="+447400344583",
-    body="Hi Gam, its that time of the week for you to fill in your questionnaire, is now a good time?"
+    body="Hi Kurren, it's that time of the week for you to fill in your questionnaire, is now a good time?"
 )
